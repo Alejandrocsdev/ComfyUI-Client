db468187354c8944de9a7d10e4a23e66c420a2a2
alejandrocsdev@gmail.com <alejandrocsdev@gmail.com>
2026-04-24 17:11:36 +0800

    add AIOFM logo in Header

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -7,10 +7,15 @@ const Header = () => {
   return (
     <header className={S.header}>
       {/* Left */}
-      <div className={S.left}></div>
+      <div className={S.left}>
+        <div className={S.logo}>
+          <span className={S.logoAi}>AI</span>
+          <div className={S.logoDivider} />
+          <span className={S.logoOfm}>OFM</span>
+        </div>
+      </div>
       {/* Right */}
       <div className={S.right}>
-        {/* <Status /> */}
       </div>
     </header>
   );
